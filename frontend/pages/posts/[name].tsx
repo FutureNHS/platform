3da2efba1d840041fd00d112b4f595d89ff06835
Tracy Wu <sytracywu@gmail.com>
2020-07-07 17:03:17 +0100

    added some space

--- frontend/pages/posts/[name].tsx
+++ frontend/pages/posts/[name].tsx
@@ -18,24 +18,27 @@ const FirstPost = ({ greeting }: { greeting: object }) => {
   const [inputValue, setInputValue] = useState("");
   return (
     <>
-      <div>Enter a name</div>
+      <section>
+        <div>Enter a name</div>
 
-      <div>{greeting}</div>
-      <input
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          window.location.href = `/posts/${inputValue}`;
-        }}
-      >
-        greet
-      </button>
-
-      <Link href="/">
-        <a>home</a>
-      </Link>
+        <div>{greeting}</div>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            window.location.href = `/posts/${inputValue}`;
+          }}
+        >
+          greet
+        </button>
+      </section>
+      <section>
+        <Link href="/">
+          <a>home</a>
+        </Link>
+      </section>
     </>
   );
 };
