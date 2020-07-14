6cf1945b2882ccdeceb2f50af050265b05db94a2
Tracy Wu <sytracywu@gmail.com>
2020-07-14 11:50:59 +0100

    client side working

--- frontend/pages/greetings/[name].tsx
+++ frontend/pages/greetings/[name].tsx
@@ -1,38 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import fetch from "node-fetch";
+import axios from "axios";
 
 import { GetServerSideProps } from "next";
+import { getGreeting } from "../../lib/greetings";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch(
-    `http://hello-world.hello-world/hello/${context?.params?.name}`
-  );
-
-  console.log("*****8888", typeof window);
-
-  const greeting = await res.text();
+  const nameProp = context?.params?.name || "";
+  const greeting = await getGreeting(nameProp);
 
   return {
     props: {
       greeting,
+      nameProp,
     },
   };
 };
 
-const GreetingForm = ({ greeting }: { greeting: object }) => {
+const GreetingForm = ({
+  greeting,
+  nameProp,
+}: {
+  greeting: object;
+  nameProp: string;
+}) => {
   const [inputValue, setInputValue] = useState("");
+  const [inputValueClient, setInputValueClient] = useState("");
+  const [axiosValue, setAxiosValue] = useState("");
+
+  const callAndSetGreeting = async (name: string) => {
+    try {
+      const res = await axios.get(`/hello/${name}`);
+
+      setAxiosValue(res.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    callAndSetGreeting(nameProp);
+  }, []);
+
   return (
     <>
       <section>
+        <div>Server side rendering example</div>
         <div>Enter a name</div>
-
         <div>{greeting}</div>
         <input
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-
+        {/* shallow - Update the path of the current page without rerunning */}
         <Link href="/greetings/[name]" as={`/greetings/${inputValue}`}>
           <button>greet</button>
         </Link>
@@ -41,6 +61,20 @@ const GreetingForm = ({ greeting }: { greeting: object }) => {
         <Link href="/">
           <a>home</a>
         </Link>
+      </section>
+      <section>
+        <div>Client side rendering example</div>
+        <div>Enter a name</div>
+
+        <div>{axiosValue}</div>
+        <input
+          value={inputValueClient}
+          onChange={(e) => setInputValueClient(e.target.value)}
+        />
+
+        <button onClick={() => callAndSetGreeting(inputValueClient)}>
+          greet
+        </button>
       </section>
     </>
   );
