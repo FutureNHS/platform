a0742ca938dbde10a723bb70d68d319fa2cbe47f
Tracy Wu <sytracywu@gmail.com>
2020-07-23 18:01:23 +0100

    has token in cookie

--- frontend/pages/auth/login.tsx
+++ frontend/pages/auth/login.tsx
@@ -1,30 +1,57 @@
 import React from "react";
 import { GetServerSideProps } from "next";
+import "isomorphic-fetch";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const request = context.query.request;
 
   if (!request && context.res) {
-    context.res.writeHead(301, {
+    context.res.writeHead(302, {
       Location: "/.ory/kratos/public/self-service/browser/flows/login",
     });
     context.res.end();
   }
 
+  const res = await fetch(
+    `http://127.0.0.1:4434/self-service/browser/flows/requests/login?request=${request}`
+  );
+  const formattedDetails = await res.json();
+  console.log("*********Details", formattedDetails);
+  //@ts-ignore
+  const csrfToken = formattedDetails.methods.password.config.fields.find(
+    (element) => element.name === "csrf_token"
+  ).value;
+  console.log("*********Config", JSON.stringify(formattedDetails, null, 4));
   return {
     props: {
-      inLoginFlow: !!request,
+      request,
+      csrfToken,
     },
   };
 };
-const Login = ({ inLoginFlow }: { inLoginFlow: boolean }) => {
+const Login = ({
+  request,
+  csrfToken,
+}: {
+  request: string;
+  csrfToken: string;
+}) => {
   return (
     <>
-      {inLoginFlow ? (
-        <form action="" method="get">
+      {request ? (
+        <form
+          action={`/.ory/kratos/public/self-service/browser/flows/login/strategies/password?request=${request}`}
+          method="POST"
+        >
+          <input
+            name="csrf_token"
+            type="hidden"
+            required={true}
+            value={csrfToken}
+          />
           <div>
             <label>Username: </label>
-            <input type="text" name="username" id="username" required />
+            <input type="text" name="identifier" id="identifier" required />
           </div>
           <div>
             <label>Password: </label>
