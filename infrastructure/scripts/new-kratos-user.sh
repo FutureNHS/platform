ffd619f1527dac811d4ae1f44f4b2f0dd04a1f55
David Laban <david.laban@red-badger.com>
2020-08-03 18:07:15 +0100

    script to add a new kratos user

---
+++ infrastructure/scripts/new-kratos-user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux0 pipefail
+
+ENVIRONMENT="${1:?"Please specify your email as the first parameter, e.g. jane.doe@red-badger.com"}"
+
+
+TEMPLATE='{
+  "traits": {
+    "email": "EMAIL"
+  }
+}'
+
+EMAIL=david.laban+curl@red-badger.com
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data "$(echo $TEMPLATE | sed s/EMAIL/$EMAIL/g)" \
+  http://kratos-admin.kratos/identities
