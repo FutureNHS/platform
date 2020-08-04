45a2474112fdadbf948a67265a32c3f93f26bb5b
David Laban <david.laban@red-badger.com>
2020-08-04 09:45:54 +0100

    shfmt -w .

--- infrastructure/scripts/new-kratos-user.sh
+++ infrastructure/scripts/new-kratos-user.sh
@@ -4,7 +4,6 @@ set -eux0 pipefail
 
 ENVIRONMENT="${1:?"Please specify your email as the first parameter, e.g. jane.doe@red-badger.com"}"
 
-
 TEMPLATE='{
   "traits": {
     "email": "EMAIL"
@@ -14,6 +13,6 @@ TEMPLATE='{
 EMAIL=david.laban+curl@red-badger.com
 
 curl --header "Content-Type: application/json" \
-  --request POST \
-  --data "$(echo $TEMPLATE | sed s/EMAIL/$EMAIL/g)" \
-  http://kratos-admin.kratos/identities
+	--request POST \
+	--data "$(echo $TEMPLATE | sed s/EMAIL/$EMAIL/g)" \
+	http://kratos-admin.kratos/identities
