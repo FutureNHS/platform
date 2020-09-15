bbd1eb50f66a837c2b9a806ac9c73bc6cc03e177
Tracy Wu <sytracywu@gmail.com>
2020-09-15 13:55:13 +0100

    moved image into link so that it is also clickable

--- frontend/components/Workspaces/WorkspaceDirectoryItem.tsx
+++ frontend/components/Workspaces/WorkspaceDirectoryItem.tsx
@@ -3,46 +3,42 @@ import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
 
-const StyledImg = styled.img`
-  display: block;
-  height: 44px;
-  width: 44px;
-  border-radius: 4px;
-
-  &:hover {
-    & + div h3 {
-      ${({ theme }) => `
-        color: ${theme.colorNhsukBlue};`}
-      text-decoration: underline;
-    }
-  }
-`;
-
 const StyledContainer = styled.div`
   display: flex;
   margin-bottom: 40px;
 `;
 
-const StyledTextContainer = styled.div`
-  margin: 0 25px;
-`;
-
 const StyledLink = styled.a`
   ${({ theme }) => `
-    h3 {
-      color: ${theme.colorNhsukBlack};
+  display: flex;
+  img {
+    display: block;
+    height: 44px;
+    width: 44px;
+    border-radius: 4px;
 
-      &:hover {
-        color: ${theme.colorNhsukBlue};
-        text-decoration: underline;
-        cursor: pointer;
+  &:hover {
+    & + h3 {
+      color: ${theme.colorNhsukBlue};
+      text-decoration: underline;
+      }
+    }
+  }
+  h3 {
+    margin-left: 25px;
+    color: ${theme.colorNhsukBlack};
+
+    &:hover {
+      color: ${theme.colorNhsukBlue};
+      text-decoration: underline;
+      cursor: pointer;
       }
 
-      &:active {
-        color: ${theme.colorNhsukBlack};
-        text-decoration: none;
-        background-color: ${theme.colorNhsukYellow};
-        border-bottom: 2px solid ${theme.colorNhsukBlack};
+    &:active {
+      color: ${theme.colorNhsukBlack};
+      text-decoration: none;
+      background-color: ${theme.colorNhsukYellow};
+      border-bottom: 2px solid ${theme.colorNhsukBlack};
       }
     }
  `}
@@ -56,17 +52,15 @@ interface Props {
 const WorkspaceDirectoryItem = ({ title, id }: Props) => {
   return (
     <StyledContainer>
-      <StyledImg
-        src={require("../../public/Placeholder_Workspace_Image.svg")}
-        alt="https://www.nhs.co.uk"
-      />
-      <StyledTextContainer>
-        <Link href="/workspaces/[id]" as={`/workspaces/${id}`}>
-          <StyledLink>
-            <h3>{title}</h3>
-          </StyledLink>
-        </Link>
-      </StyledTextContainer>
+      <Link href="/workspaces/[id]" as={`/workspaces/${id}`}>
+        <StyledLink>
+          <img
+            src={require("../../public/Placeholder_Workspace_Image.svg")}
+            alt="https://www.nhs.co.uk"
+          />
+          <h3>{title}</h3>
+        </StyledLink>
+      </Link>
     </StyledContainer>
   );
 };
