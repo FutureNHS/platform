2c2dec9df5553275af4ff6288347d7c5065af011
Jan Kuehle <jan.kuehle@red-badger.com>
2020-09-29 13:51:41 +0100

    Use cypress.env.json file instead of --env arg
    For some reason the CLI argument doesn't support whitespace in values

--- test/cypress/integration/frontend/workspaces/homepage.js
+++ test/cypress/integration/frontend/workspaces/homepage.js
@@ -10,7 +10,7 @@ describe("Workspace Homepage", () => {
 
   it("Shows the title", () => {
     cy.visit(`/workspaces/${Cypress.env("TEST_WORKSPACE_ID")}`);
-    cy.contains("h1", "Selenium Testing");
+    cy.contains("h1", Cypress.env("TEST_WORKSPACE_NAME"));
     cy.contains("h2", "Most recent items");
   });
 });
