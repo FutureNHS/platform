3c83d9907c36f9e70a5bc503fbbd6c02bba627a7
Tracy Wu <sytracywu@gmail.com>
2020-10-15 14:24:53 +0100

    form finally working properly

--- frontend/pages/workspaces/[workspaceId]/folders/[folderId]/upload-file.tsx
+++ frontend/pages/workspaces/[workspaceId]/folders/[folderId]/upload-file.tsx
@@ -3,12 +3,11 @@ import React, { useState } from "react";
 import { BlockBlobClient } from "@azure/storage-blob";
 import { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
-import { Input, Form, Button, ErrorMessage } from "nhsuk-react-components";
-import { useForm } from "react-hook-form/dist/index.ie11";
+import { Input, Form, Button } from "nhsuk-react-components";
+import { useForm, useFieldArray } from "react-hook-form/dist/index.ie11";
 import styled from "styled-components";
 import { Client } from "urql";
 
-import { DeleteIcon } from "../../../../../components/Icon";
 import { MainHeading } from "../../../../../components/MainHeading";
 import { NavHeader } from "../../../../../components/NavHeader";
 import { Navigation } from "../../../../../components/Navigation";
@@ -51,28 +50,48 @@ const StyledFileInfoBox = styled.div`
   ${({ theme }) => `
 background-color: ${theme.colorNhsukGrey4};
 `}
-  margin-bottom: 10px;
+  margin-bottom: 24px;
 `;
+
+const FormField = styled.div`
+  padding: 16px 20px;
+
+  #text {
+    padding-bottom: 60px;
+  }
+`;
+
+const StyledHeadingSection = styled.div`
+  display: flex;
+  direction: column;
+  justify-content: space-between;
+`;
+
+const StyledFileCount = styled.div``;
 
 const MAX_CHARS: { [key: string]: number } = {
   title: 50,
+  description: 250,
 };
 
-type FormData = {
+interface FormData {
   files: FileList;
-};
+  fileData: Array<{ title: string; description: string }>;
+}
 
 const UploadFile: NextPage<any> = ({ urqlClient }: { urqlClient: Client }) => {
   const router = useRouter();
   const workspaceId = (router.query.workspaceId || "unknown").toString();
   const folderId = (router.query.folderId || "unknown").toString();
 
-  const [remainingChars, setRemainingChars] = useState({
-    title: null,
-    description: null,
-  });
-
-  const { register, handleSubmit, errors, setError } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    errors,
+    setError,
+    control,
+    setValue,
+  } = useForm<FormData>();
 
   const [workspace] = useGetWorkspaceByIdQuery({
     variables: { id: workspaceId },
@@ -92,71 +111,87 @@ const UploadFile: NextPage<any> = ({ urqlClient }: { urqlClient: Client }) => {
 
   const backToPreviousPage = () => router.back();
 
-  const onSubmit = async ({ files }: FormData) => {
+  const onSubmit = async (formData: FormData) => {
     try {
       const { error, data } = await urqlClient
-        .query(FileUploadUrlsDocument, { count: files.length })
+        .query(FileUploadUrlsDocument, { count: formData.files.length })
         .toPromise();
       if (error) {
         throw new Error(`Failed to get upload URL: ${error.toString()}`);
       }
 
       if (data) {
-        await Promise.all(
-          data.fileUploadUrls.map(async (url, index) => {
-            try {
-              const blobClient = new BlockBlobClient(url);
-              const uploadResponse = await blobClient.uploadBrowserData(
-                files[index],
-                {
-                  maxSingleShotSize: 4 * 1024 * 1024,
+        const results: Array<boolean> = await Promise.all(
+          data.fileUploadUrls.map(
+            async (url: string, index: number): Promise<boolean> => {
+              try {
+                const blobClient = new BlockBlobClient(url);
+                const uploadResponse = await blobClient.uploadBrowserData(
+                  formData.files[index],
+                  {
+                    maxSingleShotSize: 4 * 1024 * 1024,
+                  }
+                );
+
+                if (uploadResponse.errorCode) {
+                  console.log(uploadResponse.errorCode);
+                  throw new Error(
+                    `Failed to upload file: ${uploadResponse.errorCode}`
+                  );
                 }
-              );
+                const { name: fileName, type: fileType } = formData.files[
+                  index
+                ];
+                const newTitle = formData.fileData[index].title;
 
-              if (uploadResponse.errorCode) {
-                throw new Error(
-                  `Failed to upload file: ${uploadResponse.errorCode}`
-                );
-              }
-              const { name: fileName, type: fileType } = files[index];
-              const newTitle = defaultFileNames[index];
-
-              const setMetaResponse = await blobClient.setMetadata({
-                newTitle,
-                fileName,
-              });
-
-              if (setMetaResponse.errorCode) {
-                throw new Error(
-                  `Failed to set file metadata: ${setMetaResponse.errorCode}`
-                );
-              }
-
-              const file = await createFile({
-                newFile: {
-                  description: "TBD", // TODO
+                const setMetaResponse = await blobClient.setMetadata({
+                  newTitle,
                   fileName,
-                  fileType,
-                  folder: folderId,
-                  temporaryBlobStoragePath: url,
-                  title: newTitle,
-                },
-              });
+                });
 
-              if (file.error) {
-                throw new Error(`Failed to save file: ${file.error?.message}`);
+                if (setMetaResponse.errorCode) {
+                  throw new Error(
+                    `Failed to set file metadata: ${setMetaResponse.errorCode}`
+                  );
+                }
+                if (newTitle === "title") {
+                  throw new Error("FAKE ERROR");
+                }
+                const file = await createFile({
+                  newFile: {
+                    description: formData.fileData[index].description,
+                    fileName,
+                    fileType,
+                    folder: folderId,
+                    temporaryBlobStoragePath: url,
+                    title: newTitle,
+                  },
+                });
+
+                if (file.error) {
+                  console.log("******file error", file.error);
+                  throw new Error(
+                    `Failed to save file: ${file.error?.message}`
+                  );
+                }
+                return true;
+              } catch (error) {
+                setError(`fileData[${index}].title`, {
+                  type: "server",
+                  message: error.toString(),
+                });
+                return false;
               }
-            } catch (error) {
-              setError(`title-${index}`, {
-                type: "server",
-                message: error.toString(),
-              });
             }
-          })
+          )
         );
-        // need to show loading and files need to be uploaded before router.push
+        if (results.every(Boolean)) {
+          router.push(`/workspaces/${workspaceId}/folders/${folderId}`);
+        } else {
+          console.log("RESULTS", results);
+          setResults(results);
+        }
       }
-      router.push(`/workspaces/${workspaceId}/folders/${folderId}`);
     } catch (error) {
       setError("files", {
         type: "server",
@@ -164,54 +199,43 @@ const UploadFile: NextPage<any> = ({ urqlClient }: { urqlClient: Client }) => {
       });
     }
   };
-  const FormField = styled.div`
-    padding: 16px 20px;
-    margin-bottom: 24px;
-    #text {
-      padding-bottom: 60px;
-    }
-  `;
 
-  const StyledHeadingSection = styled.div`
-    display: flex;
-    direction: column;
-    justify-content: space-between;
-  `;
+  const [results, setResults] = useState<Array<Boolean>>([]);
 
-  const StyledFileCount = styled.div``;
+  const { fields } = useFieldArray({
+    control,
+    name: "fileData",
+  });
 
-  const StyledRemoveButton = styled.button`
-    border: none;
-    background: inherit;
-    font: inherit;
-    padding: 5px;
-  `;
+  const [remainingChars, setRemainingChars] = useState<{
+    [key: string]: number;
+  }>({});
 
-  const handleCharNumber = (
+  const handleRemainingChars = (fieldType: "title" | "description") => (
     event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
+    const maxChars = MAX_CHARS[fieldType];
     setRemainingChars({
       ...remainingChars,
-      [event.currentTarget.name]:
-        MAX_CHARS[event.currentTarget.name] - event.currentTarget.value.length,
+      [event.currentTarget.name]: maxChars - event.currentTarget.value.length,
     });
   };
 
-  const [defaultFileNames, setFileNames] = useState<string[]>([]);
-
-  const handleFiles = (files: FileList) => {
-    const filenames = Object.values(files).map((file) => file.name); //['name1', 'name2']
-    setFileNames([...defaultFileNames, ...filenames]);
-  };
-
-  const removeFile = (index: any) => () => {
-    if (index > -1) {
-      defaultFileNames.splice(index, 1);
-      return setFileNames([...defaultFileNames]);
+  const handleFiles = (files: FileList | null) => {
+    if (files === null) {
+      return;
     }
+    const filenames = Object.values(files).map((file) => file.name); //['name1', 'name2']
+
+    setValue(
+      "fileData",
+      filenames.map((name) => ({
+        title: name,
+        description: "",
+      }))
+    );
   };
 
-  console.log("****defaultFilenames", defaultFileNames);
   return (
     <PageLayout>
       <NavHeader />
@@ -232,11 +256,7 @@ const UploadFile: NextPage<any> = ({ urqlClient }: { urqlClient: Client }) => {
               : folder.data?.folder.title || "No title!"}
           </MainHeading>
           <p> Fields marked with * are mandatory.</p>
-          <Form
-            id="filesUploadForm"
-            onSubmit={handleSubmit(onSubmit)}
-            autoComplete="off"
-          >
+          <Form id="filesUploadForm" onSubmit={handleSubmit(onSubmit)}>
             <StyledInput
               type="file"
               name="files"
@@ -258,84 +278,68 @@ const UploadFile: NextPage<any> = ({ urqlClient }: { urqlClient: Client }) => {
               All uploaded content must conform to the platform&apos;s{" "}
               <a href="#">Terms and Conditions</a>.
             </p>
-            {defaultFileNames &&
-              defaultFileNames.map((defaultFileName, index) => {
-                return (
-                  <>
-                    <StyledFileInfoBox>
-                      <FormField>
-                        <StyledHeadingSection>
-                          <StyledFileCount>{defaultFileName}</StyledFileCount>
-                          <StyledRemoveButton
-                            type="button"
-                            onClick={removeFile(index)}
-                          >
-                            <DeleteIcon />
-                          </StyledRemoveButton>
-                        </StyledHeadingSection>
-                        <Input
-                          type="text"
-                          name={`title[${index}]`}
-                          onBlur={(e) => {
-                            const newFileNames = defaultFileNames;
-                            newFileNames[
-                              index
-                            ] = (e.target as HTMLInputElement).value;
-                            setFileNames(newFileNames);
-                          }}
-                          defaultValue={defaultFileName}
-                          key={index}
-                          label="Enter file title*"
-                          hint="The title of your file should accurately reflect its content or audience"
-                          inputRef={register({
-                            required: true,
-                            maxLength: MAX_CHARS.title,
-                          })}
-                          error={
-                            errors.title &&
-                            `Folder name is required and cannot be longer than ${MAX_CHARS.title} characters`
-                          }
-                        />
-                        {`${
-                          remainingChars.title || MAX_CHARS.title
-                        } characters remaining`}
-                      </FormField>
-                      <FormField>
-                        <Textarea
-                          name="description"
-                          onChange={handleCharNumber}
-                          id="description"
-                          label="Enter description (optional)"
-                          error={
-                            errors.description &&
-                            `Description must be a maximum of ${MAX_CHARS.description} characters`
-                          }
-                          hint="This is the description as seen by users"
-                          inputRef={register({
-                            required: false,
-                            maxLength: MAX_CHARS.description,
-                          })}
-                        />
-                        {`${
-                          remainingChars.description || MAX_CHARS.description
-                        } characters remaining`}
-                      </FormField>
-                    </StyledFileInfoBox>
-                  </>
-                );
-              })}
+            {fields.map((item, index) => {
+              return (
+                <StyledFileInfoBox key={item.id}>
+                  <FormField>
+                    <StyledHeadingSection>
+                      <StyledFileCount>{item.title}</StyledFileCount>
+                      {results.length > 0 && (
+                        <div>{results[index].toString()}</div>
+                      )}
+                    </StyledHeadingSection>
+                    <Input
+                      type="text"
+                      name={`fileData[${index}].title`}
+                      defaultValue={item.title}
+                      label="Enter file title*"
+                      hint="The title of your file should accurately reflect its content or audience"
+                      onChange={handleRemainingChars("title")}
+                      inputRef={register({
+                        required: {
+                          value: true,
+                          message: "Title is required",
+                        },
+                        maxLength: {
+                          value: MAX_CHARS.title,
+                          message: `Title must be a maximum of ${MAX_CHARS.title} characters`,
+                        },
+                      })}
+                      error={errors.fileData?.[index]?.title?.message}
+                    />
+                    {`${
+                      remainingChars[`fileData[${index}].title`] ||
+                      MAX_CHARS.title
+                    } characters remaining`}
+                  </FormField>
+                  <FormField>
+                    <Textarea
+                      name={`fileData[${index}].description`}
+                      label="Enter description (optional)"
+                      error={errors.fileData?.[index]?.description?.message}
+                      hint="This is the description as seen by users"
+                      onChange={handleRemainingChars("description")}
+                      inputRef={register({
+                        maxLength: {
+                          value: MAX_CHARS.description,
+                          message: `Description must be a maximum of ${MAX_CHARS.description} characters`,
+                        },
+                      })}
+                    />
+                    {`${
+                      remainingChars[`fileData[${index}].description`] ||
+                      MAX_CHARS.description
+                    } characters remaining`}
+                  </FormField>
+                </StyledFileInfoBox>
+              );
+            })}
             <Button type="submit" name="submitButton">
               Uploads and continue
             </Button>
             <StyledButton secondary type="button" onClick={backToPreviousPage}>
               Discard
             </StyledButton>
-            {errors.title && (
-              <ErrorMessage>{errors.title.message}</ErrorMessage>
-            )}
-            {errors.files && (
-              <ErrorMessage>{errors.files.message}</ErrorMessage>
-            )}
           </Form>
         </PageContent>
       </ContentWrapper>
