a38e62945c05354ffe656ec075536ee30ecfef14
Jan Kuehle <jan.kuehle@red-badger.com>
2020-10-30 13:50:29 +0000

    Make shfmt happy

--- workspace-service/scripts/create-workspace-if-needed.sh
+++ workspace-service/scripts/create-workspace-if-needed.sh
@@ -19,32 +19,32 @@ WORKSPACE_DESCRIPTION=${3:-"Test workspace with title $WORKSPACE_TITLE"}
 WORKSPACE_SERVICE_GRAPHQL_ENDPOINT="$(_verify_environment_and_get_graphql_endpoint $ENVIRONMENT)"
 
 existing_workspaces=$(
-  curl \
-    --silent \
-    --show-error \
-    -XPOST \
-    $WORKSPACE_SERVICE_GRAPHQL_ENDPOINT \
-    -H 'Content-Type: application/json' \
-    -d '{"query": "{workspaces { title, id }}"}'
+	curl \
+		--silent \
+		--show-error \
+		-XPOST \
+		$WORKSPACE_SERVICE_GRAPHQL_ENDPOINT \
+		-H 'Content-Type: application/json' \
+		-d '{"query": "{workspaces { title, id }}"}'
 )
 found=$(
-  echo "$existing_workspaces" |
-    jq \
-      -r \
-      --arg title "$WORKSPACE_TITLE" \
-      '.data.workspaces | map(select(.title == $title))[0].id'
+	echo "$existing_workspaces" |
+		jq \
+			-r \
+			--arg title "$WORKSPACE_TITLE" \
+			'.data.workspaces | map(select(.title == $title))[0].id'
 )
 if [ "$found" != "null" ]; then
-  echo $found
-  exit 0
+	echo $found
+	exit 0
 fi
 
 body=$(
-  jq \
-    --null-input \
-    --arg title "$WORKSPACE_TITLE" \
-    --arg description "$WORKSPACE_DESCRIPTION" \
-    '{
+	jq \
+		--null-input \
+		--arg title "$WORKSPACE_TITLE" \
+		--arg description "$WORKSPACE_DESCRIPTION" \
+		'{
 			"query": "mutation CreateWorkspace($title: String!, $description: String!) { createWorkspace(newWorkspace: { title: $title,  description: $description }) { id } }",
 			"variables": {
 				"title": $title,
@@ -53,13 +53,13 @@ body=$(
 		}'
 )
 response=$(
-  curl \
-    --silent \
-    --show-error \
-    -XPOST \
-    $WORKSPACE_SERVICE_GRAPHQL_ENDPOINT \
-    -H 'x-user-auth-id: feedface-0000-0000-0000-000000000000' \
-    -H 'Content-Type: application/json' \
-    -d "$body"
+	curl \
+		--silent \
+		--show-error \
+		-XPOST \
+		$WORKSPACE_SERVICE_GRAPHQL_ENDPOINT \
+		-H 'x-user-auth-id: feedface-0000-0000-0000-000000000000' \
+		-H 'Content-Type: application/json' \
+		-d "$body"
 )
 echo "$response" | jq -r '.data.createWorkspace.id'
