605e3234334551930618482738144501e5cbcc1e
Tracy Wu <sytracywu@gmail.com>
2020-11-06 14:02:59 +0000

    stub data and initial tests working

--- frontend/cypress/integration/frontend/workspaces/members.js
+++ frontend/cypress/integration/frontend/workspaces/members.js
@@ -7,4 +7,21 @@ describe("Workspace Members page", () => {
     cy.contains("h1", "Workspace members");
     cy.contains("p", "This is a list of all workspace members.");
   });
+
+  context("Desktop - 990px+", () => {
+    beforeEach(() => {
+      cy.viewport(1200, 1080);
+    });
+
+    it("Shows list of admins and members on desktop", () => {
+      cy.visit(`workspaces/${Cypress.env("TEST_WORKSPACE_ID")}/members`);
+      cy.get("tr")
+        .eq(1)
+        .within(() => {
+          cy.get("td").eq(0).contains("Lisa Pink");
+          cy.get("td").eq(1).contains("a", "lisa.pink@example.com");
+          cy.get("td").eq(2).get(`svg[width="24"]`);
+        });
+    });
+  });
 });
