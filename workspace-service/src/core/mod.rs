7c2af959466a1f1f0c2d31681c875a29a6604af9
Stuart Harris <stuart.harris@red-badger.com>
2020-11-16 13:16:13 +0000

    comment

--- workspace-service/src/core/mod.rs
+++ workspace-service/src/core/mod.rs
@@ -6,6 +6,9 @@ pub mod team;
 pub mod user;
 pub mod workspace;
 
+/// `RepoFactoryImpl` holds a `Transaction<'a>` and hands out short-lived [Whatever]Repos.
+/// These `Repo`s hold an `&'r mut Transaction` to make DB queries with, and then let go of
+/// it as soon as they go out of scope.
 #[cfg_attr(test, mockall::automock)]
 pub trait RepoFactory<'a> {
     fn folder<'r>(&'r mut self) -> Box<dyn FolderRepo + Send + 'r>
